13cca7fbb8bce7fd98f2050b635f9dc90a1cad32
CBforeva <cbforeva@gmail.com>
2013-10-21 14:11:58 +0200

    Created a new box, with Puppet 3.3. - Also added puppet module install calls. - MySQL and PostgreSQL works nicely.

--- slc6/manifests/init_mysql.pp
+++ slc6/manifests/init_mysql.pp
@@ -1,5 +1,6 @@
 ### This init.pp file installs MySQL.
 ### The following Puppet Module is required: puppetlabs/mysql
+### Also it's dependencies: puppetlabs/stdlib
 
 group { "puppet":
   ensure => "present",
@@ -12,35 +13,37 @@ file { '/etc/motd':
               Managed by Puppet to install and configure a MySQL Server \n",
 }
 
-class { 'mysql::server': 
-  config_hash => {
-    'root_password' => 'testPass', 
-    'bind_address'  => 'localhost',
-    'port'          => '3306',
-  },
-}
+# Full default settings:
+#include '::mysql::server'
 
+# my.cnf -> [mysqld]
+class { 'mysql::server': 
+  override_options => {
+    'mysqld' => { 'max_connections' => '100',
+                  'max_allowed_packet' => '100M',
+    }
+  }
+}
+->
 mysql::db { 'testdb':
   user     => 'testUser',
   password => 'testPass',
   host     => 'localhost',
   grant    => ['all'],
-  require  => Class['mysql::server'],
+  require  => Class['::mysql::server'],
 }
-
-#database_user { 'testUser@%':
-#  ensure        => present,
-#  password_hash => mysql_password('testPass'),
-#  require       => Class['mysql::server'],
-#}
-
-#database { 'testdb':
-#  ensure  => present,
-#  charset => 'utf8',
-#  require => Type[''],
-#}
-
-#database_grant { 'testdb@%/testUser':
-#  privileges => [all],
-#}
+->
+database_user { 'testUser@%':
+  ensure        => present,
+  password_hash => mysql_password('testPass'),
+}
+->
+database { 'testdb':
+  ensure  => present,
+  charset => 'utf8',
+}
+->
+database_grant { 'testdb@%/testUser':
+  privileges => [all],
+}
 
--- slc6/manifests/init_postgresql.pp
+++ slc6/manifests/init_postgresql.pp
@@ -1,5 +1,6 @@
 ### This init.pp file installs PostgreSQL.
 ### The following Puppet Module is required: puppetlabs/postgresql
+### Also it's dependencies: puppetlabs/stdlib, firewall, apt and concat
 
 group { "puppet":
   ensure => "present",
@@ -12,23 +13,16 @@ file { '/etc/motd':
               Managed by Puppet to install and configure a PostgreSQL Server \n",
 }
 
-class { 'postgresql::server': 
-  config_hash => {
-    'ip_mask_deny_postgres_user' => '0.0.0.0/32',
-    'ip_mask_allow_all_users'   => '0.0.0.0/0',
-    'listen_addresses'          => '*',
-    'ipv4acls'                  => ['all'],
-    'manage_redhat_firewall'    => true,
-    'manage_pg_hba_conf'        => false,
-    'postgres_password'         => 'testPass',
-  },
+class { 'postgresql::server':
+  ip_mask_allow_all_users   => '0.0.0.0/0',
+  listen_addresses          => '*',
+  ipv4acls                  => ['all'],
+  manage_firewall           => true,
+  manage_pg_hba_conf        => false,
+  postgres_password         => 'testPass',
 }
 ->
-postgresql::database_user { 'testUser':
-  password_hash => 'testPass',
-}
-->
-postgresql::db { 'testdb':
+postgresql::server::db { 'testdb':
   user     => 'testUser',
   password => 'testPass',
 }
