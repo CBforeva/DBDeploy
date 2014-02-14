efe1513f45774e696e609c90a3bffe3772ba72c0
CBforeva <cbforeva@gmail.com>
2014-02-14 14:36:42 +0100

    working version.

--- slc6/manifests/ganglia/init_ganglia_web.pp
+++ slc6/manifests/ganglia/init_ganglia_web.pp
@@ -1,5 +1,9 @@
 ### This puppet manifest installing the Ganglia Monitoring's web interface with gmetad.
-# In order to use, you need: puppet module install jhoblitt/ganglia
+# On RedHat based distros ganglia web monitoring has some issues with SELinux. We'll disable it
+# before the installation. 
+# In order to use, you need the following puppet modules installed:
+# puppet module install jhoblitt/ganglia
+# puppet module install spiette/selinux
 
 $clusters = [
   {  name     => 'PerfTestCluster', 
@@ -7,9 +11,13 @@ $clusters = [
   },
 ]
 
+class { 'selinux':
+  mode => 'permissive',
+}
+->
 class { 'ganglia::web':
-      $ganglia_ip = '127.0.0.1',
-      $ganglia_port = 8652,
+  ganglia_ip => '127.0.0.1',
+  ganglia_port => 8652,
 }
 ->
 class{ 'ganglia::gmetad':
