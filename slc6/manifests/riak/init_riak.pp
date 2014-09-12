43992ea3998c789ff69297074b60e8768dedf427
CBforeva <cbforeva@gmail.com>
2014-09-12 10:47:44 +0200

    Some riak options.

--- slc6/manifests/riak/init_riak.pp
+++ slc6/manifests/riak/init_riak.pp
@@ -34,7 +34,30 @@ class { 'riak':
 
   http_ip   => '0.0.0.0',
   http_port => '8098',
+
   # For the full config please check the module's init.pp file.
+  # For frequent settings, some additional options are added here:
+
+  # kernel settings
+  # epmd_listen_min => 0,
+  # epmd_listen_max => 65535,
+
+  # riak_kv
+  # storage_backend                => 'riak_kv_bitcask_backend',
+  # anti_entropy                   => 'on',
+  # anti_entropy_build_limit       => {
+  #   num_builds   => 1,
+  #   per_timespan => 3600000,
+  # },
+  # anti_entropy_expire            => 604800000,
+  # anti_entropy_concurrency       => 2,
+  # anti_entropy_tick              => 15000,
+  # anti_entropy_data_dir          => '/var/lib/riak/anti_entropy',
+  # anti_entropy_leveldb_opts      => {
+  #   write_buffer_size => 4194304,
+  #   max_open_files    => 20,
+  # },
+
 }
 ->
 service { 'iptables':
