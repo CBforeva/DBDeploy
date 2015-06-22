9990b3218b91648c4290824b0cef0b9649b3eff7
CBforeva <cbforeva@gmail.com>
2015-06-22 15:46:54 +0200

    Small fixes and added readme.

--- slc6/manifests/oracle/init_ora11g.pp
+++ slc6/manifests/oracle/init_ora11g.pp
@@ -1,12 +1,15 @@
 
+$puppetDownloadMntPoint = "/root/"
+
 oradb::installdb{ '112010_Linux-x86-64':
-        version      => '11.2.0.1', 
-        file         => 'linux.x64_11gR2_database',
-        databaseType => 'SE',
-        oracleBase   => '/oracle',
-        oracleHome   => '/oracle/product/11.2/db',
-        user         => 'oracle',
-        group        => 'dba',
-        downloadDir  => '/install',  
+  version                => '11.2.0.1', 
+  file                   => 'linux.x64_11gR2_database',
+  databaseType           => 'SE',
+  oracleBase             => '/oracle',
+  oracleHome             => '/oracle/product/11.2/db',
+  user                   => 'oracle',
+  group                  => 'dba',
+  downloadDir            => '/install',
+  puppetDownloadMntPoint => $puppetDownloadMntPoint,
 }
 
