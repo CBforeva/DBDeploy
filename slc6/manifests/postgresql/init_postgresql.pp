44f4644559ad84c22527e5510d8d8f2f0c906320
justpili <just.pili@gmail.com>
2015-07-24 16:14:08 +0200

    Updated postgresql manifest for version control.

--- slc6/manifests/postgresql/init_postgresql.pp
+++ slc6/manifests/postgresql/init_postgresql.pp
@@ -12,45 +12,20 @@ file { '/etc/motd':
               Managed by Puppet to install and configure PostgreSQL Server \n",
 }
 
+class { 'postgresql::globals':
+  manage_package_repo => true,
+  version             => '9.4',
+}->
 class { 'postgresql::server':
   ip_mask_allow_all_users   => '0.0.0.0/0',
   listen_addresses          => '*',
   ipv4acls                  => ['host all all 0.0.0.0/0 trust'],
-  manage_firewall           => true,
+  #manage_firewall           => true,
   manage_pg_hba_conf        => true,
   postgres_password         => 'testPass',
 }
 
-postgresql::server::config_entry { 'shared_buffers':
-  value => '2048MB',
-}
-
-postgresql::server::config_entry { 'work_mem':
-  value => '100MB',
-}
-
-postgresql::server::config_entry { 'effective_cache_size':
-  value => '4096MB',
-}
-
-postgresql::server::config_entry { 'fsync':
-  value => 'off',
-}
-
-postgresql::server::config_entry { 'full_page_writes':
-  value => 'off',
-}
-
-postgresql::server::config_entry { 'synchronous_commit':
-  value => 'off',
-}
-
-postgresql::server::db { 'testdb':
-  user     => 'testUser',
-  password => 'testPass',
-}
-->
-service { 'iptables':
-  ensure => 'stopped',
-}
-
+#postgresql::server::db { 'testdb':
+#  user     => 'testUser',
+#  password => 'testPass',
+#}
