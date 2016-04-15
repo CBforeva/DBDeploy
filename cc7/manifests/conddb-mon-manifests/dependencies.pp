f81b082e12eb4bc7786c763fced80eea8323fdb2
Roland <cbforeva@gmail.com>
2016-04-15 14:30:11 +0200

    Newer version of the monitoring prod site backup.

--- cc7/manifests/conddb-mon-manifests/dependencies.pp
+++ cc7/manifests/conddb-mon-manifests/dependencies.pp
@@ -11,7 +11,7 @@
 
 $omdPackage     = "omd-1.30.x86_64"
 $rpmName        = "labs-consol-stable.rhel7.noarch.rpm"
-$prodBackupName = "prod-backup-20160316.tar.gz"
+$prodBackupName = "prod-backup-20160415.tar.gz"
 $chmkBackupName = "checkmk-agent-local.tar.gz"
 
 #$toFetch = [
